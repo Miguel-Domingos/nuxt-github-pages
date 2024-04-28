ced51d67859224dad1ac94ba9921f07faca32178
Miguel Domingos <mighueldomingos@gmail.com>
2024-04-28 20:30:54 +0200

    add baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
-    // baseURL: "/nuxt-github-pages/",
+    baseURL: "/nuxt-github-pages/",
     // buildAssetsDir: "assets",
   },
   modules: ["@nuxtjs/tailwindcss"],
